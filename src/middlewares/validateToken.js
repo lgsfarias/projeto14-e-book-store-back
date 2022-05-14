015612cf7f37985a4eee1f2705cea49d610fdc3a
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-05-13 23:56:09 -0300

    Add getKinds endpoint

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,19 +1,20 @@
-import jwt from "jsonwebtoken";
-import { ObjectId } from "mongodb";
+import jwt from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
 
-import db from "../config/dbConnect.js";
+import db from '../config/dbConnect.js';
 
 export async function validateToken(req, res, next) {
-
     const validateTokenFormat = req.headers.authorization.slice(0, 7);
-    if (validateTokenFormat != "Bearer ") {
+    if (validateTokenFormat != 'Bearer ') {
         return res.sendStatus(422);
     }
     const token = req.headers.authorization.slice(7);
     try {
         const secretKey = process.env.JWT_SECRET;
-        const {sessionId} = jwt.verify(token, secretKey);
-        const {userId} = await db.collection("sessions").findOne({ _id: new ObjectId(sessionId) });
+        const { sessionId } = jwt.verify(token, secretKey);
+        const { userId } = await db
+            .collection('sessions')
+            .findOne({ _id: new ObjectId(sessionId) });
         if (!userId) {
             return res.sendStatus(401);
         }
